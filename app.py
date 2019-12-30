3b496d12311faafe666bd736e1f6a16d71046051
woody.choi <woody.choi@kakaocorp.com>
2019-12-30 19:03:36 +0900

    no message

--- app.py
+++ app.py
@@ -1,14 +1,15 @@
 from flask import Flask, render_template
+
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+	return render_template('index.html')
+
 
 @app.route('/info')
 def info():
-    return render_template('info.html')
-
-
+	return render_template('info.html')
 
 # flask run --host=0.0.0.0 --port=7777
