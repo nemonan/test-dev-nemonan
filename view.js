05528c400964f1f97802e737edb37508dad6e2a6
knemonan <knemonan@gmail.com>
2020-01-10 03:05:39 +0900

    add7

--- view.js
+++ view.js
@@ -5,22 +5,11 @@ let subpath = params.dong +"/"+ params.line;
 let firedbref = firedb.ref('xi/' + subpath);
 
 firedbref.once('value').then(function(snapshot) {
-//    console.log(snapshot.val());
     let obj = snapshot.val();
     if (obj != null) {
-//        let list = Object.keys(obj).map(key => {
-//              return {
-//                id: key,
-//                ho: obj[key].ho,
-//                month: obj[key].month,
-//                day: obj[key].day,
-//                time: obj[key].time
-//              }
-//            });
-
         var list = []
         var keys = Object.keys(obj);
-        for (var i = 0; i<keys.length;i++) {
+        for (var i = 0; i < keys.length; i++) {
             var key = keys[i]
             var dict = {}
             dict["id"] = key;
@@ -28,26 +17,26 @@ firedbref.once('value').then(function(snapshot) {
             dict["month"] = obj[key].month;
             dict["day"] = obj[key].day;
             dict["time"] = obj[key].time;
-//            console.log(dict);
             list.push(dict);
         }
 
         list.forEach(function (o) {
 //            console.log(o.ho + " "+ o.day);
             s = document.querySelector('#m'+o.month+'-'+ o.day +'-'+ o.time);
+            // https://knemonan.firebaseio.com/xi/105/L12/-Ly7ep8Pxyw7-dFDTK60
+            fbPath = "xi/" + subpath + "/"+ o.id;
             if (s == null || s.innerText == null || s.innerText.length <= 1) {
-                s.innerHTML = " <span class='badge badge-info'>"+ o.ho +"</span>";
+                s.innerHTML = " <span class='badge badge-info' title='"+ fbPath +"'>"
+                            + o.ho +"</span>";
             }
             else {
-                s.innerHTML += " <span class='badge badge-danger'>"+ o.ho +"</span>";
+                s.innerHTML += " <span class='badge badge-danger' title='"+ fbPath + "'>"
+                            + o.ho +"</span>";
             }
         });
 
         var regCount = list.length;
-        console.log("AAA" + regCount);
-
         let viewTitle = document.querySelector('#viewTitle');
-        console.log(regCount);
         viewTitle.innerHTML += " (참여: <font color='e04000'>" + regCount + "</font>)";
     } else {
         viewTitle.innerHTML += " (미참여)";
