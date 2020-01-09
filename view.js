a3f95f9f10d15d1d5db5ba73dcc825603ffe8b35
woody.choi <woody.choi@kakaocorp.com>
2020-01-09 11:15:38 +0900

    add

--- view.js
+++ view.js
@@ -9,22 +9,35 @@ let firedbref = firedb.ref('xi/' + subpath);
 
 let vals;
 firedbref.once('value').then(function(snapshot) {
-    //console.log(snapshot.val());
+//    console.log(snapshot.val());
     let obj = snapshot.val();
     if (obj != null) {
-        let list = Object.keys(obj).map(key => {
-              return {
-                id: key,
-                ho: obj[key].ho,
-                month: obj[key].month,
-                day: obj[key].day,
-                time: obj[key].time
-              }
-            });
-        console.log(list);
+//        let list = Object.keys(obj).map(key => {
+//              return {
+//                id: key,
+//                ho: obj[key].ho,
+//                month: obj[key].month,
+//                day: obj[key].day,
+//                time: obj[key].time
+//              }
+//            });
+
+        var list = []
+        var keys = Object.keys(obj);
+        for (var i = 0; i<keys.length;i++) {
+            var key = keys[i]
+            var dict = {}
+            dict["id"] = key;
+            dict["ho"] = obj[key].ho;
+            dict["month"] = obj[key].month;
+            dict["day"] = obj[key].day;
+            dict["time"] = obj[key].time;
+//            console.log(dict);
+            list.push(dict);
+        }
 
         list.forEach(function (o) {
-            console.log(o.ho + " "+ o.day);
+//            console.log(o.ho + " "+ o.day);
             s = document.querySelector('#m'+o.month+'-'+ o.day +'-'+ o.time);
             if (s == null || s.innerText == null || s.innerText.length <= 1) {
                 s.innerHTML = " <span class='badge badge-info'>"+ o.ho +"</span>";
